5a5b575f71b804315b146de022f7098be7edb303
engrasjadazeem <engrasjadazeem@gmail.com>
2020-11-08 01:16:13 +0100

    Updates

---
+++ precache-manifest.13064a6dd8b0c601132e2d68ddb84cb5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4eaa4a11d1eb21d2c3fffc2a45b26ac",
+    "url": "/thedreamypixels-generated/index.html"
+  },
+  {
+    "revision": "3284e08698cef8dcba02",
+    "url": "/thedreamypixels-generated/static/css/main.b73b87c0.chunk.css"
+  },
+  {
+    "revision": "bab15b3cda2672af5f5d",
+    "url": "/thedreamypixels-generated/static/js/2.cae695cd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/thedreamypixels-generated/static/js/2.cae695cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3284e08698cef8dcba02",
+    "url": "/thedreamypixels-generated/static/js/main.6c1f6f7a.chunk.js"
+  },
+  {
+    "revision": "47922a8b86ec8ed475a4",
+    "url": "/thedreamypixels-generated/static/js/runtime-main.7eb4e583.js"
+  },
+  {
+    "revision": "674b682c89c1ded0f08778049c5e121f",
+    "url": "/thedreamypixels-generated/static/media/UweraAndJonas.674b682c.jpg"
+  },
+  {
+    "revision": "372e53aafdb8a84acc74874e62cbb224",
+    "url": "/thedreamypixels-generated/static/media/icomoon.372e53aa.ttf"
+  },
+  {
+    "revision": "375b780b28593a994ec9e2acce03b757",
+    "url": "/thedreamypixels-generated/static/media/icomoon.375b780b.woff"
+  },
+  {
+    "revision": "a234de7ccc980436308bf09464622599",
+    "url": "/thedreamypixels-generated/static/media/icomoon.a234de7c.eot"
+  },
+  {
+    "revision": "e84b7863e4285c272004b3f3f71fc194",
+    "url": "/thedreamypixels-generated/static/media/icomoon.e84b7863.svg"
+  }
+]);
